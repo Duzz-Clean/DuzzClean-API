d707be6bfcfc956e4b77252c62d945c5ffd0a89f
pablomariz <souzamariz27@gmail.com>
2020-05-17 00:59:50 -0300

    Rotas

--- rotas.py
+++ rotas.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify, request
-
+from model import Backend
 app = Flask(__name__)
+backend = Backend()
+''' 
+SOMENTE PARA TESTE
 
 motoristas = [
     {
@@ -70,31 +73,30 @@ avaliacao_do_usuario = [ #avaliação do usuario a respeito de UM UNICO carro
         'avaliacao': 'UMA BOSTA' #avaliacao do cliente
     }
 ]
-
+'''
 
 #mostrar json de motoristas - FUNCIONANDO
-@app.route('/motoristas', methods=['GET'])
+@app.route('/veiculos', methods=['GET'])
 def home_motoristas():
-    return jsonify(motoristas), 200
+    pass
 
 #mostrar json de usuarios - FUNCIONANDO
 @app.route('/usuarios', methods=['GET'])
 def home_usuarios():
-    return jsonify(usuarios), 200
+    pass
 
 #mostrar json de limpezas solicitadas - FUNCIONANDO
 @app.route('/nova_limpeza', methods=['GET'])
 def home_nova_limpeza():
-    return jsonify(nova_limpeza), 200
+    pass
 
 #mostrar json de avaliacao do usuario a respeito de UM UNIICO carro - FUNCIONANDO 
 @app.route('/avaliacao_do_usuario', methods=['GET'])
 def home_avalicao():
-    return jsonify(avaliacao_do_usuario), 200
-
+    pass
     
 #excluir carro - FUNCIONANDO
-@app.route('/motoristas/<string:placa>', methods=['DELETE'])
+@app.route('/veiculos/<string:placa>', methods=['DELETE'])
 def excluir_carro(placa):
     pass
 
@@ -107,10 +109,10 @@ def excluir_usuario(cpf):
 
 
 #adicionar novo motorista - FUNCIONANDO
-@app.route('/motoristas', methods=['POST'])
+@app.route('/veiculos', methods=['POST'])
 def save_motorista():
     data = request.get_json()
-    response = backend.novo_motorista(data)
+    response = backend.novo_veiculo(data)
     status = int(response['status'])
     return jsonify(data), status
 
@@ -126,12 +128,12 @@ def save_usuario():
 
 
 #pesquisar por placa - FUNCIONANDO
-@app.route('/motoristas/<string:placa>', methods=['GET'])
+@app.route('/veiculos/<string:placa>', methods=['GET'])
 def por_placa(placa):
     pass
 
 #pesquisar por ultima limpeza - ACHO QUE NÃO TEM NECESSIDADE, PQ QUANDO PESQUISAR POR PLACA JÁ VAI TER ESSE DADO (FUNCIONANDO)
-@app.route('/motoristas/ultima_limpeza/<string:ultima_limpeza>', methods=['GET'])
+@app.route('/veiculos/ultima_limpeza/<string:ultima_limpeza>', methods=['GET'])
 def ultimalimpeza(ultima_limpeza):
     pass
 
@@ -156,7 +158,7 @@ def avaliacao():
 
 
 #pesquisar situação do carro, e sua proxima manutenção.
-@app.route('/motoristas/nova_manutencao/<string:placa>', methods=['GET'])
+@app.route('/veiculos/nova_manutencao/<string:placa>', methods=['GET'])
 def nova_manutencao(placa):
     pass
 
